33cd10700fd55d28631cae37549b9bcad9e8f3cc
admin <admin@DESKTOP-99M7FA8>
2023-10-26 20:57:25 +0530

    commit and push

---
+++ src/main/java/demogithub/DemoGitClass2.java
@@ -0,0 +1,10 @@
+package demogithub;
+
+public class DemoGitClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this one I only did by learing from mam");
+	}
+
+}
