e4c303ed54fe1cdf0a820c3095adf209ef548216
admin <admin@DESKTOP-99M7FA8>
2023-10-27 19:48:21 +0530

    new package for src/test/java and new pgm

---
+++ src/test/java/demoGithub/Samplepgm1.java
@@ -0,0 +1,9 @@
+package demoGithub;
+
+public class Samplepgm1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("i ve deleted the previous package");}
+
+}
